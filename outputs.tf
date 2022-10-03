efdddb94e8e805bbfa2588f083ceaac8b883b9c7
BB <18106157+Ledermayer@users.noreply.github.com>
2022-10-03 05:42:43 +0200

    more shining

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "managed_disk_list" {
   value = {for o in azurerm_managed_disk.managed_disk : o.name => {name : o.name, id : o.id}}
+  description = "Map of created disks with name and ID."
 }
